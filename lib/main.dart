f31a4be894aff6ed4a5030487c9545f776fb517f
kenchiro <razafimaharolazaniainaelie@gmail.com>
2022-02-23 01:38:55 -0800

    automatique publish page

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:testweb/landing_screen.dart';
 
-void main() {
+Future<void> main()  async {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
